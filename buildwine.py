2b6f040c5c58f1604a7f58b2e1ae3e325ab0085a
Robert Michelsen <robert.michelsen@gmx.net>
2020-04-13 12:01:03 +0200

    buildwine: Filter for hard errors in patch command failures.
    "Reversed (or previously applied) patch detected!  Skipping patch." is not an error

--- buildwine.py
+++ buildwine.py
@@ -81,8 +81,11 @@ def patch_apply(source_path, commit_id, exclude_pattern=""):
     if not patchfile or not os.path.exists(os.path.normpath(os.path.join(source_path, patchfile))):
         sys.exit("Patch extraction of '{0}' failed, aborting!".format(commit_id))
 
-    run_command("filterdiff -p1 -x '{0}' < {1} | patch -p1 --forward --no-backup-if-mismatch".format(
-                exclude_pattern, patchfile), source_path)
+    patch_stdout = run_command_stdout("filterdiff -p1 -x '{0}' < {1} | patch -p1 --forward --no-backup-if-mismatch".format(
+                 exclude_pattern, patchfile), source_path)
+    if "failed" in patch_stdout:
+        sys.exit("Patch '{0}' failed with output '{1}', aborting!".format(patchfile, patch_stdout))
+    # "Reversed (or previously applied) patch detected!  Skipping patch." is not an error
 
 def main():
 
