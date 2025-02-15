adc442748014c13edead4fb70a98cee3fa595fc1
Robert Michelsen <robert.michelsen@gmx.net>
2025-02-15 11:59:05 +0100

    buildwine.py: Pass '-Wl,--no-insert-timestamp' to CROSSLDFLAGS for reproducible builds.

--- buildwine.py
+++ buildwine.py
@@ -445,6 +445,10 @@ def main():
         # FIXED: Wine 6.0
         if wine_version >= Version("6.0"):
             env_initialize(my_env, "CROSSCC", "clang")
+        # reproducible PE builds (related: https://bugs.winehq.org/show_bug.cgi?id=41342#c7)
+        # LLVM option added in 9.0.0 (2018): https://releases.llvm.org/9.0.0/tools/lld/docs/ReleaseNotes.html
+        if wine_version >= Version("6.0"):
+            env_initialize_or_append(my_env, "CROSSLDFLAGS", " -Wl,--no-insert-timestamp")
 
     # target arch specific build and install paths
     wine_build_target_arch32_path = ""
