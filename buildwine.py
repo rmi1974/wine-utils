a2ac1303b21e39406a70da3ce097d039c2a9ffad
Robert Michelsen <robert.michelsen@gmx.net>
2025-11-15 16:38:11 +0100

    buildwine.py: patch_apply: Fail early if required tools don't exist.

--- buildwine.py
+++ buildwine.py
@@ -85,13 +85,25 @@ def patch_apply(source_path, commit_id, exclude_pattern=""):
 
     """
 
+    # ensure required tools exist
+    for tool in ("git", "filterdiff", "patch"):
+        if not shutil.which(tool):
+            sys.exit("Required tool '{0}' not found in PATH, aborting!".format(tool))
+
     # extract the patch from Git checkout
-    patchfile = run_command_stdout("git format-patch -1 --full-index --binary {0} 2> /dev/null".format(commit_id), source_path)
+    patchfile = run_command_stdout(
+        "git format-patch -1 --full-index --binary {0} 2> /dev/null".format(commit_id),
+        source_path
+    )
     if not patchfile or not os.path.exists(os.path.normpath(os.path.join(source_path, patchfile))):
         sys.exit("Patch extraction of '{0}' failed, aborting!".format(commit_id))
 
-    patch_stdout = run_command_stdout("filterdiff -p1 -x '{0}' < {1} | patch -p1 --forward --no-backup-if-mismatch 2>&1".format(
-                 exclude_pattern, patchfile), source_path)
+    patch_stdout = run_command_stdout(
+        "filterdiff -p1 -x '{0}' < {1} | patch -p1 --forward --no-backup-if-mismatch 2>&1".format(
+            exclude_pattern, patchfile
+        ),
+        source_path
+    )
     if any(re.findall(r'failed|error:', patch_stdout, re.IGNORECASE)):
         sys.exit("Patch '{0}' failed with output '{1}', aborting!".format(patchfile, patch_stdout))
     # "Reversed (or previously applied) patch detected!  Skipping patch." is not an error
